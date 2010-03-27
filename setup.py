25ceb16279f4c74d9b4c449e2a7836d54d472356
Pankaj Pandey <pankaj86@gmail.com>
2010-03-27 15:35:23 +0530

    added setup.py for easier installation

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PyBilliards',
+      version='0.1',
+      description='A simple 2D billiards game',
+      long_description='''A simple 2D billiards game written in python using pygame''',
+      classifiers=['Topic :: Games/Entertainment'],
+      author='Prashant Agrawal & Pankaj Pandey',
+      author_email='agrawal.prash@gmail.com,pankaj86@gmail.com',
+      url='http://code.google.com/p/pybilliards/',
+      package_dir = {'': 'src'},
+      packages=['billiards'],
+      package_data={'billiards': ['data/themes/*/*']},
+      license = 'GPLv3+',
+     )
+
